21c5c060edf592b455c6ecf3c2a4298f01508555
Hm-habib <superhabib199@gmail.com>
2025-10-04 22:45:57 +0600

    file updated

--- src/component/notes/NoteListComponent/NoteListComponent.jsx
+++ src/component/notes/NoteListComponent/NoteListComponent.jsx
@@ -33,53 +33,53 @@ function NoteListComponent({ notes, onDeleteNote, onStrikeNote }) {
           <tbody className={styles.tableWidth}>
             {notes.map((item, index) => {
               return (
-                  <tr key={index}>
-                    <td> {index + 1} </td>
-                    <td
-                      onClick={() => toggleIsCompleted(index, item)}
-                      style={{ maxWidth: "500px" }}
-                      className={`text-left ${
-                        item.isCompleted ? styles.strike : undefined
+                <tr key={index}>
+                  <td> {index + 1} </td>
+                  <td
+                    onClick={() => toggleIsCompleted(index, item)}
+                    className={`text-left ${
+                      item.isCompleted ? styles.strike : undefined
+                    }`}
+                  >
+                    <h5>Title: {item.title}</h5>
+                    <div
+                      className={`${styles.noteDetails} ${
+                        openRows.includes(index) ? styles.expanded : null
                       }`}
                     >
-                      <h5>Title: {item.title}</h5>
-                      <div
-                        className={`${styles.noteDetails} ${
-                          openRows.includes(index) ? styles.expanded : ""
-                        }`}
-                      ><p>Body: {item.body} </p>
-                        
-                      </div>
-                    </td>
+                      <p>Body: {item.body} </p>
+                    </div>
+                  </td>
 
-                    <td className={styles.buttonTd}>
-                      <button
-                        type="button"
-                        className="btn btn-primary btn-sm m-1"
-                        onClick={() => {
-                          toggleExpandedRow(index);
-                        }}
-                      >
-                        {openRows.includes(index) ? <span>&#8896;</span> : <span>&#9013;</span> }
-                      </button>
-                      
-                      
-                      <button type="button" className="btn btn-info btn-sm m-1">
-                        Edit
-                      </button>
-                      
-                      
-                      <button
-                        type="button"
-                        id={`delete-btn-${index}`}
-                        className="btn btn-danger btn-sm m-1"
-                        onClick={() => onDeleteNote(index)}
-                      >
-                        Delete
-                      </button>
-                      
-                    </td>
-                  </tr>
+                  <td className={styles.buttonTd}>
+                    <button
+                      type="button"
+                      className="btn btn-primary btn-sm m-1"
+                      onClick={() => {
+                        toggleExpandedRow(index);
+                      }}
+                    >
+                      {openRows.includes(index) ? (
+                        <span>&#8896;</span>
+                      ) : (
+                        <span>&#9013;</span>
+                      )}
+                    </button>
+
+                    <button type="button" className="btn btn-info btn-sm m-1">
+                      Edit
+                    </button>
+
+                    <button
+                      type="button"
+                      id={`delete-btn-${index}`}
+                      className="btn btn-danger btn-sm m-1"
+                      onClick={() => onDeleteNote(index)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
               );
             })}
           </tbody>
